391d9314a4a75c2d4e038b5b0d494fc6681bb569
Rtrdbrah <126190719+VeryProPlayer@users.noreply.github.com>
2026-06-14 15:33:37 +0200

    Update img.c

--- img.c
+++ img.c
@@ -1,1 +1,31 @@
+#include "raylib.h"
+#include <stdio.h>
 
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Error: Please provide an image path.\n");
+    return 1;
+  }
+  const char *imagePath = argv[1];
+  Image image = LoadImage(imagePath);
+
+  if (image.data == NULL) {
+    printf("Error: Could not load image '%s'\n", imagePath);
+    return 1;
+  }
+  InitWindow(image.width, image.height, "rtrdviewer");
+  Texture2D texture = LoadTextureFromImage(image);
+  UnloadImage(image); 
+
+  SetTargetFPS(60);
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawTexture(texture, 0, 0, WHITE);
+    EndDrawing();
+  }
+  UnloadTexture(texture);
+  CloseWindow();
+
+  return 0;
+}
